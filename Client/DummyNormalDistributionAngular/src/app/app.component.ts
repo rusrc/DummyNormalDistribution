57415b7936326757ea0d506332267dcf9792abac
v-Ruslan.Shcherbakov <v-Ruslan.Shcherbakov@lionbridge.com>
2018-11-17 18:14:39 +0300

    Add client

---
+++ Client/DummyNormalDistributionAngular/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  val: number;
+
+
+  handleChange(e) {
+
+    // console.log(e);
+  }
+
+  valueSelected(e) {
+    console.log(e);
+  }
+
+}
